29236c8e0a2248077ce9731802b03f82de8416c7
Andn1ght <uzzhanibek.satigul2016@yandex.ru>
2023-10-22 18:03:32 +0600

    Initializing methods for MongoDBAdapter

--- src/MongoDBAdapter.java
+++ src/MongoDBAdapter.java
@@ -1,2 +1,17 @@
-public class MongoDBAdapter {
+public class MongoDBAdapter implements Database{
+    private MongoDBDatabase mongodb;
+
+    public MongoDBAdapter(MongoDBDatabase mongodb) {
+        this.mongodb = mongodb;
+    }
+
+    @Override
+    public void connect() {
+        mongodb.connectMongoDB();
+    }
+
+    @Override
+    public void query(String query) {
+        mongodb.queryMongoDB(query);
+    }
 }
