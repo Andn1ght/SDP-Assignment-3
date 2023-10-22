ce1c31446c6c12110372847699561f0195d28def
Andn1ght <uzzhanibek.satigul2016@yandex.ru>
2023-10-22 17:55:41 +0600

    Initializing methods for adaptee class

--- src/MongoDBDatabase.java
+++ src/MongoDBDatabase.java
@@ -1,2 +1,9 @@
 public class MongoDBDatabase {
+    public void connectMongoDB() {
+        System.out.println("Connecting to MongoDB Database");
+    }
+
+    public void queryMongoDB(String query) {
+        System.out.println("Executing MongoDB query: " + query);
+    }
 }
