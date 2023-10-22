f2843086defb4c65dc9970c53c7afb1f11c55245
Andn1ght <uzzhanibek.satigul2016@yandex.ru>
2023-10-22 18:08:38 +0600

    Client code

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
 public class Main {
+
+    private static void useDatabase(Database adapter) {
+        adapter.connect();
+        adapter.query("SELECT * FROM users");
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Database mysqlAdapter = new MySQLAdapter(new MySQLDatabase());
+        Database mongodbAdapter = new MongoDBAdapter(new MongoDBDatabase());
+
+        useDatabase(mysqlAdapter);
+        useDatabase(mongodbAdapter);
     }
 }
