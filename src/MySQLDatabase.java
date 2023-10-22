1019d8fd5ed5692cd4806e8e55dabc9102cb51a3
Andn1ght <uzzhanibek.satigul2016@yandex.ru>
2023-10-22 17:53:26 +0600

    Initializing methods for adaptee class

--- src/MySQLDatabase.java
+++ src/MySQLDatabase.java
@@ -1,2 +1,9 @@
 public class MySQLDatabase {
+    public void connectMySQL() {
+        System.out.println("Connecting to MySQL Database...");
+    }
+
+    public void queryMySQL(String query) {
+        System.out.println("Executing MySQL query: " + query);
+    }
 }
