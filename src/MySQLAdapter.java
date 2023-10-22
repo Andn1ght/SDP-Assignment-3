d24cbc9dc39b71d2cdf2babbfb784c53338dc66e
Andn1ght <uzzhanibek.satigul2016@yandex.ru>
2023-10-22 18:01:03 +0600

    Initializing the methods of MySQLAdapter class

--- src/MySQLAdapter.java
+++ src/MySQLAdapter.java
@@ -1,2 +1,17 @@
-public class MySQLAdapter {
+public class MySQLAdapter implements Database{
+    private MySQLDatabase mysql;
+
+    public MySQLAdapter(MySQLDatabase mysql) {
+        this.mysql = mysql;
+    }
+
+    @Override
+    public void connect() {
+        mysql.connectMySQL();
+    }
+
+    @Override
+    public void query(String query) {
+        mysql.queryMySQL(query);
+    }
 }
